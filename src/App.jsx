97ae02b17786d10bcaba7c367638f53094f9cd07
RitikMalik59 <ritikmalik59@gmail.com>
2024-11-14 18:09:16 +0530

    Fixed the box-shadow on hover issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   const categories = [
@@ -17,13 +18,6 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState("animation");
   const [query, setQuery] = useState("");
   const [filteredMovies, setFilteredMovies] = useState([]);
-  const [hovered, setHovered] = useState(false);
-  const toggleHover = () => {
-    console.log("hover");
-    console.log(hovered);
-
-    setHovered(!hovered);
-  };
 
   // Fetch movies from the selected category
   useEffect(() => {
@@ -92,36 +86,28 @@ function App() {
       <div className="row">
         {filteredMovies.map((movie) => (
           <div className="col-md-4 col-lg-3 mb-4" key={movie.id}>
-            <div
-              className={hovered ? "box-shadow" : ""}
-              onMouseEnter={toggleHover}
-              onMouseLeave={toggleHover}
-            >
-              <div className="card h-100">
-                <img
-                  src={
-                    movie.posterURL ||
-                    "https://c8.alamy.com/comp/2ACA4KA/black-film-shadow-2ACA4KA.jpg"
-                  }
-                  className="card-img-top"
-                  alt={movie.posterURL}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{movie.title}</h5>
-                  <p className="card-text">
-                    {movie.plot
-                      ? movie.plot.slice(0, 100) + "..."
-                      : "No description available."}
-                  </p>
-                  <a
-                    href={"https://www.google.com/search?q=" + movie.imdbId}
-                    target="_blank"
-                  >
-                    <small className="text-muted">
-                      imdbId : {movie.imdbId}
-                    </small>
-                  </a>
-                </div>
+            <div className="card h-100">
+              <img
+                src={
+                  movie.posterURL ||
+                  "https://c8.alamy.com/comp/2ACA4KA/black-film-shadow-2ACA4KA.jpg"
+                }
+                className="card-img-top"
+                alt={movie.posterURL}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text">
+                  {movie.plot
+                    ? movie.plot.slice(0, 100) + "..."
+                    : "No description available."}
+                </p>
+                <a
+                  href={"https://www.google.com/search?q=" + movie.imdbId}
+                  target="_blank"
+                >
+                  <small className="text-muted">imdbId : {movie.imdbId}</small>
+                </a>
               </div>
             </div>
           </div>
